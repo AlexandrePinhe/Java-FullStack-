e712aa48ae33a17535bd3151e4c19570ab0faf97
Alexandre Pinheiro <alexandrepinheirosantana2010@hotmail.com>
2020-09-30 06:38:32 -0300

    Add files via upload

---
+++ JPA/src/main/java/app/Main.java
@@ -0,0 +1,60 @@
+package app;
+
+//import java.awt.List;
+//import java.util.ArrayList;
+
+//import br.ufg.inf.ctrl.excecao.FabricanteException;
+//import br.ufg.inf.ctrl.negocio.CarroNegocio;
+//import br.ufg.inf.ctrl.negocio.FabricanteNegocio;
+//import br.ufg.inf.ctrl.negocio.ModeloNegocio;
+//import br.ufg.inf.model.entidade.Carro;
+//import br.ufg.inf.model.entidade.Fabricante;
+//import br.ufg.inf.model.entidade.Modelo;
+//import br.ufg.inf.model.enumerador.TipoCarro;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		/*Fabricante fab1 = new Fabricante();
+		fab1.setFabricanteId(1);
+		fab1.setFabricanteNome("Fiat");
+		Modelo mod1 = new Modelo();
+		mod1.setModeloId(1);
+		mod1.setNomeModelo("Ka");
+		mod1.setFabricante(fab1);*/
+		
+		/*Carro car1 = new Carro();
+		car1.setIdCarro(null);
+		car1.setTipo(TipoCarro.SEDAN);
+		car1.setFabricante(fab1);
+		car1.setPlaca("KTJ-4536");
+		car1.setAno(2019);
+		car1.setCor("Azul");
+		car1.setModelo("KA");*/
+		//ModeloNegocio modelNegocio = new ModeloNegocio();
+		
+		//FabricanteNegocio fabNegocio = new FabricanteNegocio();
+		//CarroNegocio carNegocio = new CarroNegocio();
+		
+		try {
+			//modelNegocio.inserir(mod1);
+			//fabNegocio.excluir(fab1);
+			//carNegocio.inserir(car1);
+			//System.out.print("Carro inserido com sucesso!");
+			//carNegocio.encerrarConexao();
+			//System.out.println("Listando os fabricantes:");
+			//ArrayList<Fabricante> fabs = (ArrayList<Fabricante>) fabNegocio.listar();
+			//for(Fabricante f : fabs) {
+			//	System.out.println(f);
+			//}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+       
+		System.out.println("Concluido!");
+	}
+
+}
