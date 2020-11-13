19f10febe2a1c75fba0f71e29edf285cc851ad51
Alexandre Pinheiro <alexandrepinheirosantana2010@hotmail.com>
2020-11-13 17:37:24 -0300

    Add files via upload

---
+++ Angular FullStack/src/app/component/Carro/carro-list/carro-list.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarroListComponent } from './carro-list.component';
+
+describe('CarroListComponent', () => {
+  let component: CarroListComponent;
+  let fixture: ComponentFixture<CarroListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarroListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarroListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
