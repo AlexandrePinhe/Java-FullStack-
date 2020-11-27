5b4f7b6aec3b13744f20aa9b46854fbded32f925
Alexandre Pinheiro <alexandrepinheirosantana2010@hotmail.com>
2020-11-27 01:01:35 -0300

    Create fabricante-page.ts

---
+++ Angular Full-Stack 3/app/component/views/Fabricante/fabricante-page.ts
@@ -0,0 +1,21 @@
+import { fabricante } from 'src/app/models/fabricante.model';
+
+export interface FabricantePage {
+    content: fabricante[];
+    pageable: string;
+    totalElements: number;
+    last: boolean;
+    totalPages: number;
+    size: number;
+    number: number;
+    sort: Sort;
+    numberOfElements: number;
+    first: boolean;
+    empty: boolean;
+  }
+  
+  export interface Sort {
+    sorted: boolean;
+    unsorted: boolean;
+    empty: boolean;
+  }
